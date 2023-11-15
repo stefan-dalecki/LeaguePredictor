bdbb9b3d3ebd758d3f879095b41a4569b630752e
JonathanHuster1248 <87677299+JonathanHuster1248@users.noreply.github.com>
2023-11-14 22:58:28 -0700

    Begin processing team level data

--- src/format_data.py
+++ src/format_data.py
@@ -134,15 +134,25 @@ def make_ts_df(data: dict):
     return ts_data
 
 
-def get_winner(data: dict):
-    try:
-        frames = data["info"]["frames"]
-        events = frames[-1]["events"]
-        for event in events:
-            if "winningTeam" in event:
-                return event.get("winningTeam")
-    except KeyError:
-        return 0
+# def get_winner(data: dict):
+#     try:
+#         frames = data["info"]["frames"]
+#         events = frames[-1]["events"]
+#         for event in events:
+#             if "winningTeam" in event:
+#                 return event.get("winningTeam")
+#     except KeyError:
+#         return 0
+
+
+def get_winner(game: dict[str, dict]) -> int:
+    """
+    return the winning team of a game from a dict of the game
+
+    :param game: a dictionary of metadata and events in a game
+    :return: either 100 or 200 to indicate the winning team
+    """
+    return game["info"]["frames"][-1]["events"][-1]["winningTeam"]
 
 
 def make_cumulative(df: pd.DataFrame, group_cols: List[str], val_cols: List[str]):
@@ -252,7 +262,126 @@ class DummyData(DataAggregator):
 
 
 class TeamAggregator(DataAggregator):
-    pass
+    columns = {}
+    index = ["game_id", "timestamp"]
+    _EVENT_MAP = {
+        "BUILDING_KILL": {
+            "TOWER_BUILDING": "tower",
+            "INHIBITOR_BUILDING": "inhibitor",
+        },
+        "ELITE_MONSTER_KILL": {
+            "DRAGON": "dragon",
+            "RIFTHERALD": "riftherald",
+            "BARON_NASHOR": "baron",
+            "ELDER_DRAGON": "elder",
+        },
+        "CHAMPION_KILL": {
+            "killerId": "kills",
+            "assistingParticipantIds": "assists",
+            "victimId": "deaths",
+        },
+    }
+
+    DECISION_COLS = [
+        "cs",
+        "gold",
+        "tower",
+        "inhibitor",
+        "dragon",
+        "riftherald",
+        "baron",
+        "elder",
+        "kills",
+        "assists",
+        "deaths",
+    ]
+
+    def get_dict_vals(dic, vals=[], rec=True):
+        for val in dic.values():
+            if type(val) == dict:
+                get_dict_vals(val, vals)
+            else:
+                vals.append(val)
+        return vals
+
+    _PLAYER_COLS = ["cs", "gold"]
+    _TEAM_COLS = get_dict_vals(_EVENT_MAP)
+
+    def __init__(self) -> None:
+        multi_index = pd.MultiIndex.from_tuples([], names=TeamAggregator.index)
+        self.df = pd.DataFrame(columns=TeamAggregator.columns, index=multi_index)
+        self.outcomes = pd.DataFrame(columns=["outcome"], index=multi_index)
+        super().__init__()
+
+    def add_frame(self, idx: tuple[str, str], frame: list[dict]) -> pd.DataFrame:
+        events = frame["events"]
+        player_frames = frame["participantFrames"]
+
+        self.df.loc[idx, :] = 0
+
+        # Add data from player snapshots
+        for player in player_frames:
+            team = _ID_TEAM_MAP[player["participantId"]]
+            self.df.loc[idx, f"cs_{team}"] += (
+                player["jungleMinionsKilled"] + player["minionsKilled"]
+            )
+            self.df.loc[idx, f"total_gold_{team}"] += player["totalGold"]
+            # TODO: XP could be added here, but likely won't be a driving factor until there is a sizeable xp diff
+
+        # Add data from event snapshots
+        for event in events:
+            event_type = event["type"]
+            if event_type not in _EVENT_MAP:
+                continue
+            try:
+                team = (
+                    event["teamId"]
+                    if "teamId" in event
+                    else _ID_TEAM_MAP[event["killerId"]]
+                )
+            except KeyError:
+                # Event not affiliated with a team. Skip to next
+                continue
+
+            # CONTINUE HERE
+            if event_type == "CHAMPION_KILL":
+                killed_team = _ID_TEAM_MAP[event["victimId"]]
+                if killed_team == team:
+                    print("what the")
+
+                # assert killed_team != team
+
+                pos_metrics = ["killerId", "assistingParticipantIds"]
+                neg_metrics = ["victimId"]
+                ts_data.loc[i].loc[
+                    team, [_EVENT_MAP[event_type][metric] for metric in pos_metrics]
+                ] += [
+                    1,
+                    len(event.get(pos_metrics[1], [])),
+                ]
+                ts_data.loc[i].loc[
+                    killed_team,
+                    [_EVENT_MAP[event_type][metric] for metric in neg_metrics],
+                ] += [1]
+            elif event_type == "ELITE_MONSTER_KILL":
+                ts_data.loc[i].loc[
+                    team, _EVENT_MAP[event_type][event["monsterType"]]
+                ] += 1
+            elif event_type == "BUILDING_KILL":
+                ts_data.loc[i].loc[
+                    team, _EVENT_MAP[event_type][event["buildingType"]]
+                ] += 1
+
+    def format_game(self, game: dict[str, dict]) -> pd.DataFrame:
+        game_id = game["info"]["gameId"]
+        frames = game["info"]["frames"]
+        for frame in frames:
+            idx = (game_id, frame["timestamp"])
+            self.add_frame(idx, frame)
+
+    def format_json(self, raw_data: dict[str, dict]) -> pd.DataFrame:
+        for game in raw_data:
+            self.format_game(game)
 
 
 class RoleAggregator(DataAggregator):
